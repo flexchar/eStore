7a74578b7369ed177e947982287dcb2bda8fc121
Lukas <lukas@youtuberis.lt>
2020-04-24 11:01:45 +0300

    Add project files.

---
+++ Models/Drone.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eStore.Models
+{
+    public class Drone
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string model_id { get; set; }
+        public virtual Model model { get; set; }
+
+    }
+}
---
+++ Models/Model.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eStore.Models
+{
+    public class Model
+    {
+        public int id { get; set; }
+
+        [Display(Name = "Series Name")]
+        public string name { get; set; }
+
+        [ForeignKey("model_id")]
+        public IEnumerable<Drone> drones { get; set; }
+
+
+    }
+}
